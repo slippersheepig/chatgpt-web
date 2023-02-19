de1d8fd9a30275aabd1c2ebb6db5c8fad84d1edc
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-02-20 00:22:19 +0800

    refactor

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from flask import Flask, request, render_template, redirect
 
 server = Flask(__name__)
 
-def get_completion(question):
+def generate_response(prompt):
     try:
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"{question}\n",
+            prompt=prompt,
             temperature=0.8,
             max_tokens=2048,
             top_p=1,
@@ -21,21 +21,14 @@ def get_completion(question):
         return e
     return response["choices"][0].text
 
-@server.route('/chat', methods=['GET', 'POST'])
-def get_request_json():
-    if request.method == 'POST':
-        if len(request.form['question']) < 1:
-            return render_template(
-                'chat.html', question="null", res="问题不能为空")
-        question = request.form['question']
-        print("======================================")
-        print("接到请求:", question)
-        res = get_completion(question)
-        print("问题：\n", question)
-        print("答案：\n", res)
+@server.route("/")
+def home():
+    return render_template("chat.html")
 
-        return render_template('chat.html', question=question, res=str(res))
-    return render_template('chat.html', question=0)
+@server.route("/get")
+def get_bot_response():
+    user_text = request.args.get('msg')
+    return str(generate_response(user_text))
 
 if __name__ == '__main__':
     server.run(debug=False, host='0.0.0.0', port=8088)
