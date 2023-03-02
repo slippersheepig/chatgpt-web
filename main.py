80bf8f61900df2fb8dbb9f1ea72ca698e89e475c
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-03-02 14:28:34 +0800

    change to use official chatgpt api

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ def generate_response(prompt):
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
-            prompt=prompt,
-            temperature=0.9,
+            messages=[{"role": "user", "content": prompt}],
+            temperature=0.7,
             max_tokens=2048,
             top_p=1,
             frequency_penalty=0,
@@ -19,7 +19,7 @@ def generate_response(prompt):
 
         print(e)
         return e
-    return response["choices"][0].text
+    return response["choices"][0]["message"]["content"]
 
 @server.route("/chat")
 def home():
