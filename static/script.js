c28593571d64ec2ae3dc8270bfb6c9867d6515df
sheepgreen <58287293+slippersheepig@users.noreply.github.com>
2023-05-05 21:43:24 +0800

    fix scroll bug

--- static/script.js
+++ static/script.js
@@ -9,18 +9,16 @@ input.addEventListener("keydown", function (event) {
 
 // Add your JavaScript here
 document.getElementById("sendbutton").addEventListener("click", function () {
-    let loading = document.getElementById('loading');
-    loading.style.display = 'block';
     // Get the user's message from the input field
     var message = document.getElementById("chatinput").value;
+    var chatlog = document.getElementById("chatlog");
+    var response = document.createElement("div");
     if (message.length < 1) {
-        var response = document.createElement("div");
         response.innerHTML = "🤔<br>🤖<br>Message cannot be null\n问题不能为空";
         chatlog.appendChild(response);
-        // Scroll the chatlog to the bottom
-        chatlog.scrollTop = chatlog.scrollHeight;
-        loading.style.display = 'none';
+        response.scrollIntoView({ behavior: 'smooth', block: 'end' });
     } else {
+        response.innerHTML = "🤖<br>思考中，请稍后...";
         // Clear the input field
         document.getElementById("chatinput").value = "";
         // Send the message to the chatbot
@@ -29,13 +27,9 @@ document.getElementById("sendbutton").addEventListener("click", function () {
         xhr.send();
         xhr.onload = function () {
             // Append the chatbot's response to the chatlog
-            var chatlog = document.getElementById("chatlog");
-            var response = document.createElement("div");
             response.innerHTML = "🤔<br>" + message + "<br>🤖" + marked.parse(xhr.responseText);
             chatlog.appendChild(response);
-            // Scroll the chatlog to the bottom
-            chatlog.scrollTop = chatlog.scrollHeight;
-            loading.style.display = 'none';
+            response.scrollIntoView({ behavior: 'smooth', block: 'end' });
         }
     }
 });
